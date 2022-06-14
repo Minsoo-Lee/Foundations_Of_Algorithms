75a0e5e1e7a7ad7bf39d6d98503ecf3a1797947d
이민수 <minsoo@iminsuui-MacBookAir.local>
2022-06-14 14:54:04 +0900

    06.14

--- Chapter03/example/2_dynamic_bi_coeff.py
+++ Chapter03/example/2_dynamic_bi_coeff.py
@@ -1,11 +1,11 @@
 def bin(n, k):
-    B = [[0 for x in range(0, n)] for x in range(0, k)]
+    B = [[0 for x in range(0, n + 1)] for x in range(0, k + 1)]
     for i in range(0, n + 1):
         for j in range(0, min(i, k) + 1):
             if j == 0 or j == i:
                 B[i][j] = 1
             else:
-                B[i][j] = B[i - 1][j - 1] + B[i - 1] + B[i - 1][j]
+                B[i][j] = B[i - 1][j - 1] + B[i - 1][j]
     for i in range(0, n):
         for j in range(0, k):
             print(B[i][j], end = " ")
